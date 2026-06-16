398d58e3997b9a419016f996f1aeedd27f73b83e
Jannael <orlandojannael@gmail.com>
2026-06-16 17:15:22 -0600

    style: refine conic border and order component

--- src/components/order.tsx
+++ src/components/order.tsx
@@ -2,7 +2,7 @@ import type { Order } from '@/api/get-all-orders'
 
 export default function Order({ order }: { order: Order }) {
 	return (
-		<li key={order.id} className="bg-bg-secondary rounded-lg p-4">
+		<li key={order.id} className="bg-bg-secondary bg-bg rounded-lg p-4">
 			<header className="mb-[16px]">
 				<span className="text-reference font-medium">Order</span>
 				{order.orderNumber && <span className="font-inter text-txt ml-2 text-[17.3px] font-semibold">#{order.orderNumber}</span>}
