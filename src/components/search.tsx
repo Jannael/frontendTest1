5f1f16079a810f36180a56e58cd17e21b53c4e70
Jannael <orlandojannael@gmail.com>
2026-06-16 16:16:15 -0600

    docs: add comment explaining client-side filtering

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -4,7 +4,8 @@ import { SearchInput } from '@/components/search-input'
 import { OrdersList } from '@/components/orders-list'
 import type { Order } from '@/api/get-all-orders'
 
-//NOTE: The filtered in the client
+// NOTE: The filtered in the client side, because the api doesn't support search query params, and we don't want to fetch all orders every time the user types something in the search input, so we fetch all orders once and then filter them in the client side, also i was told the backend stuff was not important.
+
 interface SearchOrdersProps {
 	orders: Order[]
 }
