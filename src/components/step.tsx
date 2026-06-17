6dc30b70a72d3efb5efbc6a9b105e4f1618ea689
Jannael <orlandojannael@gmail.com>
2026-06-17 10:10:02 -0600

    refactor: extract Step component from order-track

---
+++ src/components/step.tsx
@@ -0,0 +1,30 @@
+export default function Step({
+	label,
+	isCompleted,
+	totalSteps,
+	stepIndex,
+}: {
+	label: string
+	isCompleted: boolean
+	totalSteps?: number
+	stepIndex?: number
+}) {
+	return (
+		<li className="relative flex items-start gap-4">
+			{isCompleted ? (
+				<span className="bg-primary flex size-[28px] items-center justify-center rounded-full">
+					<img src="/check.svg" className="h-[6.6px] w-[10.1px]" />
+				</span>
+			) : (
+				<span className="flex size-[28px] items-center justify-center rounded-full">
+					<img src="/disabled-bullet.svg" className="size-full" />
+				</span>
+			)}
+			<span className={`text-[15.4px] font-extrabold ${isCompleted ? 'text-txt' : 'text-txt-tertiary'}`}>{label}</span>
+
+			{totalSteps !== undefined && stepIndex !== undefined && stepIndex !== 1 && (
+				<div className={`absolute -top-[21px] left-[13px] h-[21px] w-[2px] bg-red-500 ${isCompleted ? 'bg-primary' : 'bg-txt-tertiary'}`}></div>
+			)}
+		</li>
+	)
+}
