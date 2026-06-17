e6c8b7c7fa1d1acfc4bf866f0f16c9bc4690b8f8
Jannael <orlandojannael@gmail.com>
2026-06-17 09:18:49 -0600

    style: improve order-track layout with conic border and time display

--- src/components/order-track.tsx
+++ src/components/order-track.tsx
@@ -13,7 +13,10 @@ export default function OrderTrack({ orders }: OrderTrackProps) {
 	return (
 		<section className="flex w-full flex-col items-center justify-center px-[38px]">
 			{/* i am always using the default because images are blocked from AWS */}
-			<img src="/default-profile.png" />
+			<div className="conic-border bg-bg relative mt-[100px] flex w-full items-center justify-center rounded-[20px] pt-[70px]">
+				<img src="/default-profile.png" className="absolute top-0 left-1/2 size-fit -translate-x-1/2 -translate-y-1/2 transform" />
+				<span className="text-txt mb-[40px] text-[20px] font-extrabold">10:30 PM</span>
+			</div>
 		</section>
 	)
 }
