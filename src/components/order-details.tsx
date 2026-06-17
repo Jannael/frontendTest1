bedef5853c0315fc2f2461d92a19e6f75fc793db
Jannael <orlandojannael@gmail.com>
2026-06-17 08:30:15 -0600

    feat: add dropoff section to order details

--- src/components/order-details.tsx
+++ src/components/order-details.tsx
@@ -7,8 +7,7 @@ export default function OrderDetails() {
 	const params = new URLSearchParams(window.location.search)
 	const pickupAddress = order?.destinations[0]?.address || ''
 	const status = statusMap[order?.status ?? 1] || statusMap[1]
-
-	// const dropoffAddress = order?.destinations[order?.destinations.length - 1]?.address || ''
+	const dropoffAddress = order?.destinations[order?.destinations.length - 1]?.address || ''
 
 	useEffect(() => {
 		const fetchOrders = async () => {
@@ -30,38 +29,44 @@ export default function OrderDetails() {
 
 	return (
 		<div className="bg-bg conic-border mt-[20px] ml-[34px] flex w-full items-start justify-start rounded-[20px]">
-			<div className="py-[24px] pl-[42px]">
+			<div className="flex flex-col py-[24px] pl-[42px]">
 				<header className="mb-[22px] flex flex-col">
 					<span className="text-txt font-inter text-[13px] font-semibold"> Reference: #{order.referenceNumber}</span>
 					<span className="text-txt font-inter text-[17.5px] font-semibold"> Order: #{order.orderNumber}</span>
 				</header>
 
-				<div className="flex w-full items-center">
-					<div className="bg-primary outline-primary mr-[20px] flex size-[32px] flex-grow items-center justify-center gap-2 rounded-full outline outline-offset-[4px]">
-						<img src="/truck-black-stroke.svg" alt="Truck" className="h-[17.6px] w-[26.5px]" />
+				<div className="relative flex flex-col items-center justify-center before:absolute before:left-4 before:h-1/5 before:w-[1px] before:bg-gradient-to-b before:from-[#D9D9D9] before:to-[#0F1315]">
+					<div className="mb-[30px] flex w-full items-center">
+						<div className="bg-primary outline-primary mr-[20px] flex size-[32px] flex-grow items-center justify-center gap-2 rounded-full outline outline-offset-[4px]">
+							<img src="/truck-black-stroke.svg" alt="Truck" className="h-[17.6px] w-[26.5px]" />
+						</div>
+						<div className="flex max-w-[215px] min-w-0 flex-grow flex-col gap-[2px] px-3">
+							<span className="text-txt-tertiary text-[8px] font-semibold">PICKUP</span>
+							<span className="text-txt text-[15.5px] font-semibold">{order.route.pickup}</span>
+							<span className="text-reference w-full truncate text-[12.5px] font-medium">{pickupAddress}</span>
+							<div className="flex h-[10px] items-center gap-2 text-[10px]">
+								<span className={`${status.color} h-[10px] w-[10px] rounded-full`} />
+								<span className={`text-txt h-[10px]`}>{status.label}</span>
+							</div>
+						</div>
 					</div>
-					<div className="flex max-w-[215px] min-w-0 flex-grow flex-col gap-[2px] px-3">
-						<span className="text-txt-tertiary text-[8px] font-semibold">PICKUP</span>
-						<span className="text-txt text-[15.5px] font-semibold">{order.route.pickup}</span>
-						<span className="text-reference w-full truncate text-[12.5px] font-medium">{pickupAddress}</span>
-						<div className="flex h-[10px] items-center gap-2 text-[10px]">
-							<span className={`${status.color} h-[10px] w-[10px] rounded-full`} />
-							<span className={`text-txt h-[10px]`}>{status.label}</span>
+
+					<div className="flex w-full items-center">
+						<div className="mr-[20px] flex size-[32px] flex-grow items-center justify-center gap-2 rounded-full outline outline-offset-[4px]">
+							<span className="border-margin-gray size-[26px] rounded-full border"></span>
+						</div>
+						<div className="flex max-w-[215px] min-w-0 flex-grow flex-col gap-[2px] px-3">
+							<span className="text-txt-tertiary text-[8px] font-semibold">DROPOFF</span>
+							<span className="text-txt text-[15.5px] font-semibold">{order.route.dropoff}</span>
+							<span className="text-reference w-full truncate text-[12.5px] font-medium">{dropoffAddress}</span>
+							<div className="flex h-[10px] items-center gap-2 text-[10px]">
+								<span className={`${status.color} h-[10px] w-[10px] rounded-full`} />
+								<span className={`text-txt h-[10px]`}>{status.label}</span>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-
-			{/* <div className="flex w-full items-center">
-				<div className="flex size-20 flex-grow items-center gap-2">
-					<img src="/location.svg" alt="Location" className="h-[17.6px] w-[26.5px]" />
-				</div>
-				<div className="flex min-w-0 flex-grow flex-col gap-[2px] px-3 max-w-[215px]">
-					<span className="text-txt-tertiary text-[8px] font-semibold">DROPOFF</span>
-					<span className="text-txt text-[15.5px] font-semibold">{order.route.dropoff}</span>
-					<span className="text-reference w-full truncate text-[12.5px] font-medium">{dropoffAddress}</span>
-				</div>
-			</div> */}
 		</div>
 	)
 }
