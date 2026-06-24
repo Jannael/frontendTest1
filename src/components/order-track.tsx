cf90c878121c3ca3c5ebb1f5e8e3823553a74166
Jannael <orlandojannael@gmail.com>
2026-06-23 18:41:14 -0600

    fix: image over border

--- src/components/order-track.tsx
+++ src/components/order-track.tsx
@@ -26,7 +26,7 @@ export default function OrderTrack({ orders }: OrderTrackProps) {
 		<section className="mb-[35px] flex w-full flex-col items-center justify-center px-[38px]">
 			{/* i am always using the default because images are blocked from AWS */}
 			<div className="conic-border bg-bg relative mt-[100px] flex w-full flex-col items-center justify-center rounded-[20px] pt-[70px]">
-				<img src="/default-profile.png" className="absolute top-0 left-1/2 size-[90px] size-fit -translate-x-1/2 -translate-y-1/2 transform" />
+				<img src="/default-profile.png" className="absolute top-0 left-1/2 z-10 size-[90px] size-fit -translate-x-1/2 -translate-y-1/2 transform" />
 				<span className="text-txt mb-[40px] text-[20px] font-extrabold">10:30 PM</span>
 
 				<ol className="m-auto mb-[45px] ml-[45px] flex w-full flex-col items-start justify-center gap-[21px]">
